86bfd7dcc3553a73edab5b9b299494d3a2e3d4b6
Rastko Zivkovic <rale.pablito@gmail.com>
2022-06-20 17:19:45 +0200

    add debounce

--- src/pages/Main/Search/Search.js
+++ src/pages/Main/Search/Search.js
@@ -1,22 +1,60 @@
 import { getCharacters } from "../../../api/api";
-import { Button } from "@mui/material";
+import { TextField, InputAdornment } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { useState, useEffect } from "react";
 
 const Search = () => {
-  const handleClick = async () => {
-    try {
-      const response = await getCharacters();
-      const data = response.data;
-      console.log(data);
-    } catch (err) {
-      console.log(err);
-    }
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const debounce = (callback, delay) => {
+    let timeout;
+    return (...args) => {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        callback(...args);
+      }, delay);
+    };
   };
+
+  //setting input state after user stops typing for 1 second
+  const updateDebouceInput = debounce(input => {
+    setSearchTerm(input);
+  }, 1000);
+
+  const handleChange = e => {
+    updateDebouceInput(e.target.value);
+  };
+
+  //waiting for debounce to set searchTerm before fetching data from server
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getCharacters(searchTerm);
+        const data = response.data;
+        console.log(data);
+        return;
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [searchTerm]);
 
   return (
     <>
-      <Button type="button" onClick={handleClick}>
-        Get Characters!
-      </Button>
+      <TextField
+        id="search"
+        placeholder="Search..."
+        onChange={handleChange}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+        variant="outlined"
+      />
     </>
   );
 };
